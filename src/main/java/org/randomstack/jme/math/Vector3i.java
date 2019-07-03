9e40709fb20bdc7e4383251ed3f992a65269ee24
rvandoosselaer <remy.vand@gmail.com>
2019-07-03 16:24:39 +0200

    add a basic Vector3i implementation

---
+++ src/main/java/org/randomstack/jme/math/Vector3i.java
@@ -0,0 +1,33 @@
+package org.randomstack.jme.math;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vector3i implements Serializable, Cloneable {
+
+    private int x;
+    private int y;
+    private int z;
+
+    public void set(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public Vector3i clone() {
+        try {
+            return (Vector3i) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Error cloning!");
+        }
+    }
+
+}
