a2572a22215875b09fda928939d4c782b599877b
rvandoosselaer <remy.vand@gmail.com>
2019-07-04 14:40:16 +0200

    add method to Vector3i and a unit test

--- src/main/java/org/randomstack/jme/math/Vector3i.java
+++ src/main/java/org/randomstack/jme/math/Vector3i.java
@@ -1,24 +1,57 @@
 package org.randomstack.jme.math;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 
+@Slf4j
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vector3i implements Serializable, Cloneable {
 
+    @EqualsAndHashCode.Include
     public int x;
+    @EqualsAndHashCode.Include
     public int y;
+    @EqualsAndHashCode.Include
     public int z;
 
     public void set(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public Vector3i add(int x, int y, int z) {
+        return new Vector3i(this.x + x, this.y + y, this.z + z);
+    }
+
+    public Vector3i add(Vector3i vec) {
+        if (vec == null) {
+            log.warn("Supplied vector is null, returning null.");
+            return null;
+        }
+
+        return new Vector3i(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    }
+
+    public Vector3i addLocal(int x, int y, int z) {
+        this.x += x;
+        this.y += y;
+        this.z += z;
+        return this;
+    }
+
+    public Vector3i addLocal(Vector3i vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        this.z += vec.z;
+        return this;
     }
 
     @Override
---
+++ src/test/java/org/randomstack/jme/math/Vector3iTest.java
@@ -0,0 +1,29 @@
+package org.randomstack.jme.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Vector3iTest {
+
+    @Test
+    public void testAddition() {
+        Vector3i v = new Vector3i(0, 0, 0);
+        v.addLocal(5, 3, 1);
+
+        Assertions.assertEquals(new Vector3i(5, 3, 1), v);
+
+        v.addLocal(new Vector3i(0, -2, 3));
+
+        Assertions.assertEquals(new Vector3i(5, 1, 4), v);
+
+        v = new Vector3i(1, 1, 1);
+        Vector3i result = v.add(2, 3, 4);
+
+        Assertions.assertEquals(new Vector3i(1, 1, 1), v);
+        Assertions.assertEquals(new Vector3i(3, 4, 5), result);
+
+        result = v.add(new Vector3i(3, 2, 4));
+        Assertions.assertEquals(new Vector3i(1, 1, 1), v);
+        Assertions.assertEquals(new Vector3i(4, 3, 5), result);
+    }
+}
